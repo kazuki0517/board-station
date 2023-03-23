979c875b315f9d41d093cfcb0914a09d26d26838
kazuki0517 <kh2328pkh@gmail.com>
2023-03-23 21:51:59 +0900

    header component をimport

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import Threads from './components/Threads';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Threads />
     </div>
   );
