5dfccbca2d68d1bdd2853338eca1039321dbbef2
kazuki0517 <kh2328pkh@gmail.com>
2023-03-26 14:43:09 +0900

    スレッド作成時の警告文と作成成功文の追加

--- src/components/CreateThread.jsx
+++ src/components/CreateThread.jsx
@@ -1,19 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
-import { useNavigate, History } from 'react-router-dom';
 
 const StyledWrapper = styled.div`
   background-color: #F6F1F1;
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
   margin: 30px;
   height: 300px;
   box-shadow: 0px 0px 15px -5px #777777;
+  align-items: center;
   border-radius: 10px;
 `;
+
+const StyledWarning = styled.p`
+  margin: 0;
+  padding: 0;
+  color: red;
+`
+
+const StyledSuccess = styled(StyledWarning)`
+  color: #146C94;
+`
 
 const StyledInput = styled.input`
   display: block;
@@ -36,31 +46,49 @@ font-weight: bold;
 width: 200px;
 box-shadow: 0px 0px 10px -5px #777777;
 border-radius: 10px;
-`
+`;
 
 const StyledBackButton = styled(StyledButton)`
   width: 100px;
-`
+`;
 
 const CreateThread = () => {
  const [threadName, setThreadName] = useState("");
+ const [formError, setFormError] = useState("");
+ const [formSuccess, setFormSuccess] = useState("");
  const navigate = useNavigate();
 
  const createNewThread = async () => {
+   setFormError(validate(threadName));
    await axios.post('https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads', {
    title: threadName
-  }).then((res) => {
-   console.log(res);
-  }).catch((error) => {
-   console.log(error);
-  });
-  navigate("/");
+  })
+  .then((res) => {
+    if(res.status === 200) {
+      setFormSuccess("スレッドを作成しました！")
+    }
+  }).catch((error)=> {
+    console.log(error);
+  })
+  setThreadName("");
+  };
+
+ 
+
+ const validate = (value) => {
+  let error = "";
+  if(!value) {
+  error = "スレッド名を入力してください！";
+  }
+  return error
  }
 
   return (
     <StyledWrapper>
+      {formError ? <StyledWarning>{formError}</StyledWarning> : ""}
+      {formSuccess ? <StyledSuccess>スレッドを作成しました！</StyledSuccess> : ""}
       <h2>スレッド新規作成</h2>
-      <StyledInput type="text" placeholder="スレッド名を入力" onChange={(e) => setThreadName(e.target.value)} />
+      <StyledInput required value={threadName} type="text" name="name" placeholder="スレッド名を入力" onChange={(e) => setThreadName(e.target.value)} />
       <StyledButton onClick={createNewThread}>作成</StyledButton>
       <StyledBackButton onClick={()=> navigate("/")}>ホームへ戻る</StyledBackButton>
     </StyledWrapper>
